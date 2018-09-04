063b6b1225f444a1a802899113d3823d6c5e80fd
RDIL <34555510+RDIL@users.noreply.github.com>
2018-09-04 14:58:29 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SAPI-Maker',
+    version='v1.0.0',
+    packages=[''],
+    url='https://github.com/RDIL/SAPI-Maker',
+    license='MIT License',
+    author='RDIL',
+    author_email='contactspaceboom@gmail.com',
+    description='Make your Windows computer speak to you!'
+)
