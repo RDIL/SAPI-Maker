ded1327a989375f771f042ede3196711f98f9277
RDIL <34555510+RDIL@users.noreply.github.com>
2018-09-26 08:23:42 -0400

    automated the thing

--- Main.py
+++ Main.py
@@ -9,7 +9,8 @@ codeMode = input("Press 1 then press enter if this is your first time running th
 if(codeMode == "1"):
     texttowrite = input("What would you like your computer to say? Enter it here: ")
 else:
-    texttowrite = input("Please open the file labled SAPI.vbs and delete any text inside it.  Then type in what you want to be said here: ")
+    sapifile = open("SAPI.vbs", "w")
+    texttowrite = input("Type in what you want to be said here: ")
 sapifile = open("SAPI.vbs", "a")
 sapifile.write('Dim message, sapi\n')
 sapifile.write('message="'+texttowrite+'"\n')
